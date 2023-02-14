65cd870de3fdcdc9ac002594253a34bea522a093
Samarthpkori <124175205+Samarthpkori@users.noreply.github.com>
2023-02-14 18:57:35 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,26 @@
+# n1,n2=2,7
+# while(n1!=n2):
+#     if(n1>n2):
+#         n1=n1-n2
+#     else:
+#         n2=n2-n1
+# print(n1)
+
+# a,b,c=2,5,6
+# print(a/b)
+
+# n=420
+# if(n%2==0):
+#     print("even",n)
+# else:
+#     print("odd",n)
+
+# import math
+# a,b,c,r=2,2,2,2
+# pi=3.142
+# s=(a+b+c)/2
+# areacri=pi*r*r
+# print(areacri)
+# areatri=math.sqrt(s*(s-a)*(s-b)*(s-c))
+# print(areatri)
+
