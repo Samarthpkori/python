95a7e563797c997af572f6c4b927e2798a92e9da
Samarthpkori <124175205+Samarthpkori@users.noreply.github.com>
2023-02-16 18:21:37 +0530

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,54 @@
+# a,b=5,5
+# print(a+b)
+# import math
+# a=4
+# sqrt= a**0.5
+# print(sqrt)
+# print(math.sqrt(a))
+
+# h=4
+# b=5
+# area=0.5*b*h
+# print(area)
+
+
+# p,t,r=5000,2,10
+# si=(p*t*r)/100
+# print(si)
+
+# r=2
+# pi=22/7
+# areacir=pi*r*r
+# print(areacir)
+
+# si=(5000*2*10)/100
+# print(si)
+
+# print("the area of square",5*5)
+
+# n=420
+# if(n%2==0):
+#     print("even",n)
+# else:
+#     print("odd",n)
+
+# num =5
+# num=int(input("enter the no.:5"))
+# factorial=1
+# if num < 0:
+#        print("Sorry, factorial does not exist for negative numbers")
+# elif num == 0:
+#    print("The factorial of 0 is 1")
+# else:
+#    for i in range(1,num + 1):
+#        factorial = factorial*i
+#    print("The factorial of",num,"is",factorial)
+
+
+# p= 1200   
+# t= 2      
+# r= 5.4    
+# a=p*(1+(r/100))**t  
+# ci=a-p  
+# print(ci)
+
